571c264b249e370d73ec0ecd33533ec37300c9e2
pho <pho@192.168.2.2>
2017-11-08 09:12:41 +0100

    preload sql scripts with required game data

---
+++ server/etc/sql/truncate.sql
@@ -0,0 +1,6 @@
+truncate descent.gamehero cascade;
+truncate descent.campaign cascade;
+truncate descent.questencounter cascade;
+truncate descent.player cascade;
+truncate descent.heroselection cascade;
+truncate descent.mapfield cascade;
