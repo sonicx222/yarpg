6cd63392eca0ee05e7cddb93ca7bfb668736d321
pho <pho@192.168.2.2>
2017-11-18 20:09:19 +0100

    added DTO for administrative tasks

---
+++ DescentShared/src/main/java/de/pho/descent/shared/dto/WsEntityRequest.java
@@ -0,0 +1,28 @@
+package de.pho.descent.shared.dto;
+
+/**
+ *
+ * @author pho
+ */
+public class WsEntityRequest {
+ 
+    private String type;
+    private long id;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+}
