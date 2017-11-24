0de811cd4720cc2ddb59c274bdf167062facaf45
pho <pho@192.168.2.2>
2017-11-24 15:44:34 +0100

    start of hero ability implementation

---
+++ DescentShared/src/main/java/de/pho/descent/shared/heroability/AbilityGrisban.java
@@ -0,0 +1,16 @@
+package de.pho.descent.shared.heroability;
+
+import de.pho.descent.shared.model.hero.GameHero;
+
+/**
+ *
+ * @author pho
+ */
+public class AbilityGrisban implements HeroAbility {
+
+    @Override
+    public void perform(GameHero hero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ DescentShared/src/main/java/de/pho/descent/shared/heroability/HeroAbility.java
@@ -0,0 +1,12 @@
+package de.pho.descent.shared.heroability;
+
+import de.pho.descent.shared.model.hero.GameHero;
+
+/**
+ *
+ * @author pho
+ */
+@FunctionalInterface
+public interface HeroAbility {
+    public void perform(GameHero hero);
+}
