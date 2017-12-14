8de4e548b2627417f235d8a05df60dee11c884ba
pho <pho@192.168.2.2>
2017-12-14 15:10:14 +0100

    added message controls for ingame UI

--- DescentFXClient/src/main/java/de/pho/descent/fxclient/presentation/game/hero/HeroGamePresenter.java
+++ DescentFXClient/src/main/java/de/pho/descent/fxclient/presentation/game/hero/HeroGamePresenter.java
@@ -3,24 +3,39 @@ package de.pho.descent.fxclient.presentation.game.hero;
 import static de.pho.descent.fxclient.MainApp.showError;
 import static de.pho.descent.fxclient.MainApp.switchFullscreenScene;
 import de.pho.descent.fxclient.business.auth.Credentials;
+import de.pho.descent.fxclient.business.ws.MessageClient;
 import de.pho.descent.fxclient.business.ws.QuestClient;
 import de.pho.descent.fxclient.business.ws.ServerException;
 import de.pho.descent.fxclient.presentation.general.GameDataModel;
 import de.pho.descent.fxclient.presentation.startmenu.StartMenuView;
+import de.pho.descent.shared.dto.WsMessage;
 import de.pho.descent.shared.dto.WsQuestEncounter;
 import de.pho.descent.shared.model.hero.GameHero;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javax.inject.Inject;
 
 /**
@@ -28,6 +43,8 @@ import javax.inject.Inject;
  * @author pho
  */
 public class HeroGamePresenter implements Initializable {
+
+    private static final Logger LOGGER = Logger.getLogger(HeroGamePresenter.class.getName());
 
     @FXML
     private ScrollPane mapScrollPane;
@@ -59,28 +76,39 @@ public class HeroGamePresenter implements Initializable {
     @FXML
     private Label labelHeroicFeat;
 
+    @FXML
+    private ListView<WsMessage> chatListView;
+
+    @FXML
+    private TextField messageTextField;
+
     @Inject
     private Credentials credentials;
 
     @Inject
     private GameDataModel gameDataModel;
+
     private final String IMAGE_SUFFIX = ".png";
+
+    private LinearGradient gradientMenuItem;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        gradientMenuItem = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[]{
+            new Stop(0, Color.DARKVIOLET),
+            new Stop(0.1, Color.BLACK),
+            new Stop(0.9, Color.BLACK),
+            new Stop(1, Color.DARKVIOLET)
+        });
+
         // load active quest encounter of current campaign
         if (gameDataModel.getCurrentQuestEncounter() == null) {
             loadQuestEncounter();
         }
 
         setupHeroSheet();
-
-        // temporary
-        mapScrollPane.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.ESCAPE) {
-                switchFullscreenScene(e, new StartMenuView());
-            }
-        });
+        setupMessages();
     }
 
     private void loadQuestEncounter() {
@@ -96,6 +124,34 @@ public class HeroGamePresenter implements Initializable {
         if (wsQuestEncounter != null) {
             gameDataModel.setCurrentQuestEncounter(wsQuestEncounter);
         }
+    }
+
+    private void setupMessages() {
+        // messages
+        chatListView.setItems(gameDataModel.getCampaignMessages());
+        chatListView.setCellFactory(param -> new ListCell<WsMessage>() {
+            private Text text;
+
+            @Override
+            protected void updateItem(WsMessage item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null || item.getMessageText() == null) {
+                    setText(null);
+                } else {
+                    text = new Text(item.getUsername() + ": " + item.getMessageText());
+                    text.setWrappingWidth(chatListView.getPrefWidth());
+                    setGraphic(text);
+                }
+            }
+        });
+        // ENTER key on message text area
+        messageTextField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                handleSendMessage(event);
+            }
+        });
+        updateCampaignMessages();
     }
 
     private void setupHeroSheet() {
@@ -136,11 +192,107 @@ public class HeroGamePresenter implements Initializable {
     }
 
     @FXML
-    public void handleBackNavigation(MouseEvent event) {
+    public void handleOnMouseEntered(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
 
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(gradientMenuItem);
+                } else if (node instanceof Text) {
+                    ((Text) node).setFill(Color.WHITE);
+                }
+            });
+        }
     }
 
-    private void resizeGridPane() {
+    @FXML
+    public void handleOnMouseExited(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
 
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(Color.BLACK);
+                } else if (node instanceof Text) {
+                    ((Text) node).setFill(Color.DARKGREY);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleOnMousePressed(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
+
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(Color.DARKVIOLET);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleOnMouseReleased(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
+
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(gradientMenuItem);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleRefresh(MouseEvent event) {
+        LOGGER.info("HeroGamePresenter: handleRefresh()");
+        updateCampaignMessages();
+    }
+
+    @FXML
+    public void handleNavigationBack(MouseEvent event) {
+        LOGGER.info("HeroGamePresenter: handleNavigationBack()");
+        switchFullscreenScene(event, new StartMenuView());
+    }
+
+    @FXML
+    public void handleSendMessage(MouseEvent event) {
+        // cast Event to prevent recursive stack overflow
+        handleSendMessage((Event) event);
+    }
+
+    private void handleSendMessage(Event event) {
+        if ((messageTextField.getText() != null) && (!messageTextField.getText().isEmpty())) {
+            WsMessage message = null;
+            try {
+                message = MessageClient.postMessage(credentials.getPlayer(), gameDataModel.getCurrentCampaign(), messageTextField.getText());
+            } catch (ServerException ex) {
+                showError(ex);
+            }
+            if (message != null) {
+                updateCampaignMessages();
+                messageTextField.clear();
+            }
+        }
+    }
+
+    private void updateCampaignMessages() {
+        gameDataModel.getCampaignMessages().clear();
+
+        List<WsMessage> campaignMessages = null;
+        try {
+            campaignMessages = MessageClient.getCampaignMessages(credentials.getUsername(), credentials.getPassword(), gameDataModel.getCurrentCampaign());
+        } catch (ServerException ex) {
+            showError(ex);
+        }
+
+        if (campaignMessages != null && !campaignMessages.isEmpty()) {
+            gameDataModel.getCampaignMessages().addAll(campaignMessages);
+            chatListView.scrollTo(gameDataModel.getCampaignMessages().size() - 1);
+        }
     }
 }
--- DescentFXClient/src/main/java/de/pho/descent/fxclient/presentation/game/overlord/OverlordGamePresenter.java
+++ DescentFXClient/src/main/java/de/pho/descent/fxclient/presentation/game/overlord/OverlordGamePresenter.java
@@ -1,22 +1,36 @@
 package de.pho.descent.fxclient.presentation.game.overlord;
 
-import de.pho.descent.fxclient.presentation.game.hero.*;
 import static de.pho.descent.fxclient.MainApp.showError;
 import static de.pho.descent.fxclient.MainApp.switchFullscreenScene;
 import de.pho.descent.fxclient.business.auth.Credentials;
+import de.pho.descent.fxclient.business.ws.MessageClient;
 import de.pho.descent.fxclient.business.ws.QuestClient;
 import de.pho.descent.fxclient.business.ws.ServerException;
 import de.pho.descent.fxclient.presentation.general.GameDataModel;
 import de.pho.descent.fxclient.presentation.startmenu.StartMenuView;
+import de.pho.descent.shared.dto.WsMessage;
 import de.pho.descent.shared.dto.WsQuestEncounter;
 import java.net.URL;
+import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javax.inject.Inject;
 
 /**
@@ -25,8 +39,16 @@ import javax.inject.Inject;
  */
 public class OverlordGamePresenter implements Initializable {
 
+    private static final Logger LOGGER = Logger.getLogger(OverlordGamePresenter.class.getName());
+
     @FXML
     private ScrollPane mapScrollPane;
+
+    @FXML
+    private ListView<WsMessage> chatListView;
+
+    @FXML
+    private TextField messageTextField;
 
     @Inject
     private Credentials credentials;
@@ -34,20 +56,48 @@ public class OverlordGamePresenter implements Initializable {
     @Inject
     private GameDataModel gameDataModel;
 
+    private LinearGradient gradientMenuItem;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        // temporary
-        mapScrollPane.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.ESCAPE) {
-                switchFullscreenScene(e, new StartMenuView());
-            }
+        gradientMenuItem = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, new Stop[]{
+            new Stop(0, Color.DARKVIOLET),
+            new Stop(0.1, Color.BLACK),
+            new Stop(0.9, Color.BLACK),
+            new Stop(1, Color.DARKVIOLET)
         });
 
         // load active quest encounter of current campaign
         if (gameDataModel.getCurrentQuestEncounter() == null) {
             loadQuestEncounter();
         }
+
+        // messages
+        chatListView.setItems(gameDataModel.getCampaignMessages());
+        chatListView.setCellFactory(param -> new ListCell<WsMessage>() {
+            private Text text;
+
+            @Override
+            protected void updateItem(WsMessage item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null || item.getMessageText() == null) {
+                    setText(null);
+                } else {
+                    text = new Text(item.getUsername() + ": " + item.getMessageText());
+                    text.setWrappingWidth(chatListView.getPrefWidth());
+                    setGraphic(text);
+                }
+            }
+        });
+        // ENTER key on message text area
+        messageTextField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                handleSendMessage(event);
+            }
+        });
+        updateCampaignMessages();
     }
 
     private void loadQuestEncounter() {
@@ -66,7 +116,107 @@ public class OverlordGamePresenter implements Initializable {
     }
 
     @FXML
-    public void handleBackNavigation(MouseEvent event) {
+    public void handleOnMouseEntered(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
 
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(gradientMenuItem);
+                } else if (node instanceof Text) {
+                    ((Text) node).setFill(Color.WHITE);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleOnMouseExited(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
+
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(Color.BLACK);
+                } else if (node instanceof Text) {
+                    ((Text) node).setFill(Color.DARKGREY);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleOnMousePressed(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
+
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(Color.DARKVIOLET);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleOnMouseReleased(MouseEvent event) {
+        if (event.getSource() instanceof StackPane) {
+            StackPane menuItem = (StackPane) event.getSource();
+
+            menuItem.getChildren().forEach((node) -> {
+                if (node instanceof Rectangle) {
+                    ((Rectangle) node).setFill(gradientMenuItem);
+                }
+            });
+        }
+    }
+
+    @FXML
+    public void handleRefresh(MouseEvent event) {
+        LOGGER.info("OverlordGamePresenter: handleRefresh()");
+        updateCampaignMessages();
+    }
+
+    @FXML
+    public void handleNavigationBack(MouseEvent event) {
+        LOGGER.info("OverlordGamePresenter: handleNavigationBack()");
+        switchFullscreenScene(event, new StartMenuView());
+    }
+
+    @FXML
+    public void handleSendMessage(MouseEvent event) {
+        // cast Event to prevent recursive stack overflow
+        handleSendMessage((Event) event);
+    }
+
+    private void handleSendMessage(Event event) {
+        if ((messageTextField.getText() != null) && (!messageTextField.getText().isEmpty())) {
+            WsMessage message = null;
+            try {
+                message = MessageClient.postMessage(credentials.getPlayer(), gameDataModel.getCurrentCampaign(), messageTextField.getText());
+            } catch (ServerException ex) {
+                showError(ex);
+            }
+            if (message != null) {
+                updateCampaignMessages();
+                messageTextField.clear();
+            }
+        }
+    }
+
+    private void updateCampaignMessages() {
+        gameDataModel.getCampaignMessages().clear();
+
+        List<WsMessage> campaignMessages = null;
+        try {
+            campaignMessages = MessageClient.getCampaignMessages(credentials.getUsername(), credentials.getPassword(), gameDataModel.getCurrentCampaign());
+        } catch (ServerException ex) {
+            showError(ex);
+        }
+
+        if (campaignMessages != null && !campaignMessages.isEmpty()) {
+            gameDataModel.getCampaignMessages().addAll(campaignMessages);
+            chatListView.scrollTo(gameDataModel.getCampaignMessages().size() - 1);
+        }
     }
 }
