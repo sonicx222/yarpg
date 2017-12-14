304779066ac4b1c0793ab2fa1bbe4cba4bb57fb9
pho <pho@192.168.2.2>
2017-12-14 15:12:32 +0100

    better Exception message for non existant game maps

--- server/src/main/java/de/pho/descent/web/map/MapService.java
+++ server/src/main/java/de/pho/descent/web/map/MapService.java
@@ -4,11 +4,13 @@ import de.pho.descent.shared.model.map.GameMap;
 import de.pho.descent.shared.model.map.MapField;
 import de.pho.descent.shared.model.quest.Quest;
 import de.pho.descent.shared.model.quest.QuestPart;
+import de.pho.descent.web.exception.NotFoundException;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -26,11 +28,17 @@ public class MapService implements Serializable {
         return em.find(GameMap.class, mapID);
     }
 
-    public GameMap getMapByQuestAndPart(Quest quest, QuestPart part) {
-        return em.createNamedQuery(GameMap.FIND_MAP_BY_QUEST_AND_PART, GameMap.class)
+    public GameMap getMapByQuestAndPart(Quest quest, QuestPart part) throws NotFoundException {
+        TypedQuery<GameMap> query = em.createNamedQuery(GameMap.FIND_MAP_BY_QUEST_AND_PART, GameMap.class)
                 .setParameter(GameMap.QUEST_PARAM, quest)
-                .setParameter(GameMap.QUESTPART_PARAM, part)
-                .getSingleResult();
+                .setParameter(GameMap.QUESTPART_PARAM, part);
+        List<GameMap> results = query.getResultList();
+
+        if (results == null || results.isEmpty()) {
+            throw new NotFoundException("GameMap for quest: " + quest + "/" + part + " not found");
+        }
+
+        return results.get(0);
     }
 
     public List<MapField> getPlayerSpawnFieldsByMapId(long mapId) {
