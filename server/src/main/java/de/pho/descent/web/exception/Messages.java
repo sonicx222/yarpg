13f481ea27c3ad316162182ed82404e74703d5a2
pho <pho@192.168.2.2>
2018-10-11 10:36:43 +0200

    more refactoring: exception messages for action handling

---
+++ server/src/main/java/de/pho/descent/web/exception/Messages.java
@@ -0,0 +1,13 @@
+package de.pho.descent.web.exception;
+
+/**
+ *
+ * @author pho
+ */
+public class Messages {
+    public static final String WRONG_QUEST_ID = "Wrong quest encounter id";
+    public static final String NO_ACTIONS_LEFT = "No action points left";
+    public static final String WRONG_UNIT_FOR_ACTION = "Wrong active unit for action";
+    public static final String INVALID_START_POS_FOR_ACTION = "Invalid starting position for action";
+    public static final String INVALID_TARGET_POS_FOR_ACTION = "Invalid target position for action";
+}
